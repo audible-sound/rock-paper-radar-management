175f10be38181e853e5dcd09603f3b68d1b77ace
TalonExe <110237665+TalonExe@users.noreply.github.com>
2024-07-21 10:34:32 +0800

    fixed minor bugs

--- src/components/Layouts/ContentLayout.jsx
+++ src/components/Layouts/ContentLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ContentLayout = ({ children }) => {
     return (
-        <div className='flex flex-col h-screen w-screen'>
+        <div className='flex flex-col h-screen w-full'>
             {children}
         </div>
     )
--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = ({ title }) => {
     return (
-        <div className='w-screen border-solid border-2 p-4 '>
+        <div className='w-full border-solid border-2 p-4 '>
             <span className='text-2xl'><b>{title}</b></span>
         </div>
     )
