4a5633e5d2b56d2ee5a239602d30b4564817984a
TalonExe <110237665+TalonExe@users.noreply.github.com>
2024-07-23 07:58:56 +0800

    added logout button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,31 +4,42 @@ import { Link } from 'react-router-dom'
 import Mapicon from '../assets/images/MapIcon.svg'
 import CalendarIcon from '../assets/images/CalendarIcon.svg'
 import face from "../assets/images/Lucas.jpg"
-
+import logoutIcon from '../assets/images/LogoutIcon.svg'
 
 const Navbar = () => {
     return (
-        <ul className="menu bg-base-100 rounded-box p-0 pt-8">
-            <li>
-                <Link to="/profile" className='tooltip tooltip-right pt-4 pb-4 ' data-tip="Profile">
-                    <div className='avatar'>
-                        <div className='ring-primary ring-offset-base-100 w-8 rounded-full ring ring-offset-2'>
-                            <img src={face} alt="" />
+        <div className='flex flex-col h-full justify-between'>
+            <ul className="menu bg-base-100 rounded-box p-0 pt-8">
+                <li>
+                    <Link to="/profile" className='tooltip tooltip-right pt-4 pb-4 ' data-tip="Profile">
+                        <div className='avatar'>
+                            <div className='ring-primary ring-offset-base-100 w-8 rounded-full ring ring-offset-2'>
+                                <img src={face} alt="" />
+                            </div>
                         </div>
-                    </div>
-                </Link>
-            </li>
-            <li>
-                <Link to="/posts" className="tooltip tooltip-right pt-2 pb-2" data-tip="Posts">
-                    <img src={Mapicon} alt="" className='w-8' />
-                </Link>
-            </li>
-            <li>
-                <Link to="/posts-create" className="tooltip tooltip-right pt-2 pb-2" data-tip="Travel Plan">
-                    <img src={CalendarIcon} alt="" className='w-10' />
-                </Link>
-            </li>
-        </ul>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/posts" className="tooltip tooltip-right pt-2 pb-2" data-tip="Posts">
+                        <img src={Mapicon} alt="" className='w-8' />
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/posts-create" className="tooltip tooltip-right pt-2 pb-2" data-tip="Travel Plan">
+                        <img src={CalendarIcon} alt="" className='w-10' />
+                    </Link>
+                </li>
+            </ul>
+            <ul className='menu bg-base-100 rounded-box p-0'>
+                <li>
+                    <Link to="/signup" className='tooltip tooltip-right pt-2 pb-2 mb-8' data-tip="Log Out">
+                        <img src={logoutIcon} alt="" className='w-10' />
+                    </Link>
+                </li>
+            </ul>
+
+        </div>
+
     )
 }
 
