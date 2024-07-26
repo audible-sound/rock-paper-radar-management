56b8186896f8b18e498538a62056f36139b6e8dd
TalonExe <110237665+TalonExe@users.noreply.github.com>
2024-07-26 10:35:11 +0800

    added sticky sidenav

--- src/components/Layouts/ContentLayout.jsx
+++ src/components/Layouts/ContentLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ContentLayout = ({ children }) => {
     return (
-        <div className='flex flex-col bg-[#EDE8F5] bg-opacity-50 h-screen w-full'>
+        <div className='flex flex-col h-dvh bg-[#EDE8F5] bg-opacity-50 w-full pl-[4.5rem]'>
             {children}
         </div>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import Mapicon from '../assets/images/MapIcon.svg'
@@ -7,30 +7,44 @@ import face from "../assets/images/Lucas.jpg"
 import logoutIcon from '../assets/images/LogoutIcon.svg'
 
 const Navbar = () => {
+    const [scroll, setScroll] = useState(0)
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [])
+
+    const handleScroll = () => {
+        setScroll(window.scrollY)
+    }
+
+
     return (
-        <div className='flex flex-col h-full justify-between'>
-            <ul className="menu bg-base-100 rounded-box p-0 pt-8">
-                <li>
-                    <Link to="/profile" className='tooltip tooltip-right pt-4 pb-4 ' data-tip="Profile">
-                        <div className='avatar'>
-                            <div className='ring-primary ring-offset-base-100 w-8 rounded-full ring ring-offset-2'>
-                                <img src={face} alt="" />
+        <div className='flex flex-col h-full'>
+            <ul className="fixed menu bg-base-100 h-dvh p-0 pt-8 justify-between">
+                <div>
+                    <li>
+                        <Link to="/profile" className='tooltip tooltip-right pt-4 pb-4 ' data-tip="Profile">
+                            <div className='avatar'>
+                                <div className='ring-primary ring-offset-base-100 w-8 rounded-full ring ring-offset-2'>
+                                    <img src={face} alt="" />
+                                </div>
                             </div>
-                        </div>
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/posts" className="tooltip tooltip-right pt-2 pb-2" data-tip="Posts">
-                        <img src={Mapicon} alt="" className='w-8' />
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/posts-create" className="tooltip tooltip-right pt-2 pb-2" data-tip="Travel Plan">
-                        <img src={CalendarIcon} alt="" className='w-10' />
-                    </Link>
-                </li>
-            </ul>
-            <ul className='menu bg-base-100 rounded-box p-0'>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/posts" className="tooltip tooltip-right pt-2 pb-2" data-tip="Posts">
+                            <img src={Mapicon} alt="" className='w-8' />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/posts-create" className="tooltip tooltip-right pt-2 pb-2" data-tip="Travel Plan">
+                            <img src={CalendarIcon} alt="" className='w-10' />
+                        </Link>
+                    </li>
+                </div>
+
+
                 <li>
                     <Link to="/signin" className='tooltip tooltip-right pt-2 pb-2 mb-8' data-tip="Log Out">
                         <img src={logoutIcon} alt="" className='w-10' />
