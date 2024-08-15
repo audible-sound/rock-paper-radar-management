324a0c5923780ff4b7073ea6f30bc21ef666d307
char-kuey-teow <charlottechenzishan@gmail.com>
2024-08-16 00:18:05 +0800

    edit faq

--- src/components/UserGuide/UserGuideSection.jsx
+++ src/components/UserGuide/UserGuideSection.jsx
@@ -7,7 +7,7 @@ const UserGuideSection = ({ section, content = [], onSelect }) => {
       {/* section title */}
       <div className="collapse collapse-arrow">
         <input type="radio" name="user-guide-accordion" defaultChecked />
-        <div className="collapse-title text-xl font-medium active:font-bold text-balance pl-12">
+        <div className="collapse-title text-xl font-medium active:font-bold text-balance pl-11">
           {section}
         </div>
 
@@ -16,7 +16,7 @@ const UserGuideSection = ({ section, content = [], onSelect }) => {
             <button
               key={index}
               type="button"
-              className="pl-9 mb-2 focus:bg-[#7091E6] w-full text-left focus:text-slate-100 ease-in-out duration-200 rounded-md"
+              className="pl-7 mb-2 focus:bg-[#7091E6] w-full text-left focus:text-slate-100 ease-in-out duration-200 rounded-md"
               onClick={() => onSelect([item])}
             >
               {item.title}
