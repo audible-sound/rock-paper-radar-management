80d64fb88fbc55049f216c5c06f3dfb989b16893
TalonExe <110237665+TalonExe@users.noreply.github.com>
2024-08-15 09:24:29 +0800

    fixed bugs

--- src/components/Posts/TopBar.jsx
+++ src/components/Posts/TopBar.jsx
@@ -6,7 +6,12 @@ const TopBar = () => {
   const [location, setLocation] = useState('');
   return (
     <div className='flex flex-row justify-between bg-white border-solid border-x-2 px-8 py-2'>
-      <Input />
+      <Input 
+        left={"Title"}
+        placeholder={"Enter Title of Your Post"}
+        registerInput={"postTitle"}
+        required='*Required'
+      />
       <div className='flex items-center'>
         <span className="text-xl mx-10"><b>{location}</b></span>
         <SearchInput setLocation={setLocation} />
